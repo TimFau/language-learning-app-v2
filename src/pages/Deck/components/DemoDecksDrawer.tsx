fcfc1138c9ef58f20a9fd0b14c09d76d6ca3e67b
TimFau <tim@timfau.com>
2025-05-21 21:59:43 -0400

    Add Skeleton Loading for DemoDecksDrawer

--- src/pages/Deck/components/DemoDecksDrawer.tsx
+++ src/pages/Deck/components/DemoDecksDrawer.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Drawer, Typography, IconButton }  from '@mui/material';
+import { Drawer, Typography, IconButton, Skeleton, Card, CardContent, CardActions }  from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import deckService from '../../../services/deckService';
 import DeckCard from '../../../components/DeckCard';
@@ -19,24 +19,36 @@ interface DemoDeckDrawerProps {
     onClose: (event: React.UIEvent<HTMLElement>) => void,
 }
 
+// Artificial delay for skeleton testing
+const ENABLE_ARTIFICIAL_DELAY = true;
+const ARTIFICIAL_DELAY_MS = 2000;
+
 export default function DemoDecks(props: DemoDeckDrawerProps) {
     const [error, setError] = useState('');
     const [items, setItems] = useState<listItem[]>([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-
-        deckService.getDemoDecks().then(
+        setLoading(true);
+        const fetchData = () => deckService.getDemoDecks().then(
             (result) => {
                 setItems(result);
+                // setLoading(false);
             },
             // Note: it's important to handle errors here
             // instead of a catch() block so that we don't swallow
             // exceptions from actual bugs in components.
             (error) => {
                 setError(error);
+                setLoading(false);
                 console.log(error);
             }
-    )
+        );
+        if (ENABLE_ARTIFICIAL_DELAY) {
+            setTimeout(fetchData, ARTIFICIAL_DELAY_MS);
+        } else {
+            fetchData();
+        }
     }, [])
   
     if (error) {
@@ -71,12 +83,37 @@ export default function DemoDecks(props: DemoDeckDrawerProps) {
             </div>
             <div className="drawer-content">
                 <div className="decks-container">
-                    {items.map(item => (
-                        <DeckCard
-                            item={item}
-                            key={item.id.toString()}
-                        />
-                    ))}
+                    {loading ? (
+                        Array.from({ length: 3 }).map((_, idx) => (
+                            <Card className="deck-card deck-card-skeleton" key={idx}>
+                                <CardContent>
+                                    <div className="card-content-top">
+                                        <div className="deck-info">
+                                            <Skeleton variant="circular" width={24} height={24} className="deck-skeleton-icon" />
+                                            <Skeleton variant="text" width={60} height={20} className="deck-skeleton-lang" />
+                                        </div>
+                                        {/* Add back if categories become required attribute */}
+                                        {/* <div className="deck-categories">
+                                            <Skeleton variant="rounded" width={50} height={28} className="deck-skeleton-chip" />
+                                        </div> */}
+                                    </div>
+                                    <Skeleton variant="text" width="70%" height={32} className="deck-name deck-skeleton-name" />
+                                </CardContent>
+                                <CardActions style={{ justifyContent: 'flex-end' }}>
+                                    <div>
+                                        <Skeleton variant="text" width={80} height={30} className="deck-skeleton-cta" />
+                                    </div>
+                                </CardActions>
+                            </Card>
+                        ))
+                    ) : (
+                        items.map(item => (
+                            <DeckCard
+                                item={item}
+                                key={item.id.toString()}
+                            />
+                        ))
+                    )}
                 </div>
             </div>
         </Drawer>
